86cdf07a00807d40d2f4f2832ade046a7502998f
moimoimoi <meingayehun@gmnail.com>
2026-04-08 14:47:39 +0530

    Initial project setup

---
+++ src/com/trainapp/InitializeTrainDisplayConsistSummary.java
@@ -0,0 +1,17 @@
+package com.trainapp;
+
+import java.util.*;
+
+public class InitializeTrainDisplayConsistSummary {
+    public static void main(String args[]){
+        System.out.println("==================================================");
+        System.out.println("=========Train Consist Management App=============");
+        System.out.println("==================================================");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Train Initialized Successfully");
+        System.out.println("Initialize number of bogies: "+ trainConsist.size());
+        System.out.println("System Ready For further operations.");
+    }
+}
